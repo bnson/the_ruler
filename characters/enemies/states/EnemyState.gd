753fcc9235b8c238039ebd66fd1a0bb7a8163c08
BUI NGOC SON <bnson@live.com>
2025-07-24 10:37:51 +0700

    Update EnemyState.gd

---
+++ characters/enemies/states/EnemyState.gd
@@ -0,0 +1,12 @@
+class_name EnemyState extends Node
+
+var state_machine: Node = null
+
+func enter() -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func physics_update(_delta: float) -> void:
+	pass
