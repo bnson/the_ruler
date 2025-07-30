d491747c452f24342b55c279ad7641e371209b9a
BUI NGOC SON <bnson@live.com>
2025-07-30 18:28:11 +0700

    Create Item.gd

---
+++ src/scripts/Item.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name Item
+
+@export var id: String
+@export var name: String
+@export var description: String
+@export var type: String
+@export var value: int
+@export var atlas_texture: AtlasTexture
