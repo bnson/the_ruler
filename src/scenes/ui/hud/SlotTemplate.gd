3cd1e5b40889b814a26bd7b83369e91d4cef77b2
BUI NGOC SON <bnson@live.com>
2025-08-05 18:31:58 +0700

    Update SlotTemplate.gd

--- src/scenes/ui/hud/SlotTemplate.gd
+++ src/scenes/ui/hud/SlotTemplate.gd
@@ -1,12 +1,26 @@
-extends TextureButton
+extends Panel
 
-@onready var icon: TextureRect = $Icon
-@onready var quantity_label: Label = $Quantity
+signal slot_clicked(item: Item)
+
+@onready var selected_panel: Panel = $PanelSelected
+@onready var icon: TextureRect = $TextureButton/Icon
+@onready var quantity_label: Label = $TextureButton/Quantity
+
+var current_item: Item = null
 
 func set_item(item: Item, quantity: int):
+	current_item = item
+	set_selected(false)
+	
 	if item:
 		icon.texture = item.atlas_texture
 		quantity_label.text = str(quantity)
 	else:
 		icon.texture = null
 		quantity_label.text = ""
+
+func set_selected(selected: bool) -> void:
+	selected_panel.visible = selected
+
+func _on_texture_button_pressed() -> void:
+	emit_signal("slot_clicked", self)
