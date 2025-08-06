c6cadc86c7702c473930d062df66509082732f15
BUI NGOC SON <bnson@live.com>
2025-08-06 17:48:23 +0700

    Update AmiceGroverState.gd

--- src/characters/npcs/amice_grover/AmiceGroverState.gd
+++ src/characters/npcs/amice_grover/AmiceGroverState.gd
@@ -1,17 +1,5 @@
-extends Resource
+# AmiceGroverState.gd
+extends NPCState
 class_name AmiceGroverState
 
-var name: String = "Amice Grover"
-var love: int = 0
-var trust: int = 0
-var favorite_gifts: Array[String] = ["Tea", "Sewing Kit"]
-var given_gifts: Array[String] = []
-
-func receive_gift(item_name: String) -> void:
-	if item_name in favorite_gifts:
-		love += 5
-		trust += 3
-	else:
-		love += 1
-		trust += 1
-	given_gifts.append(item_name)
+# Nếu cần có logic riêng thì override, không thì dùng chung từ NPCState
