b6a3a7adbb68caee06af786f3594400d6e49759f
BUI NGOC SON <bnson@live.com>
2025-08-15 18:44:20 +0700

    Update PlayerUi.gd

--- src/autoload/PlayerUi.gd
+++ src/autoload/PlayerUi.gd
@@ -1,15 +1,20 @@
 ### 📄 PlayerUi.gd
 extends CanvasLayer
 
+@onready var player_info_ui: Control = $MarginContainer2/PlayerInfoUi
 @onready var inventory_ui: Control = $CenterContainer3/InventoryUI
 @onready var dialogue_ui: DialogueUi = $CenterContainer4/DialogueUi
-@onready var player_info_ui: Control = $MarginContainer2/PlayerInfoUi
+@onready var shop_ui: Control = $MarginContainer3/ShopUi
+
+@onready var joystick : Joystick = $CenterContainer1/Joystick
+@onready var attack_buttons : AttackButtons = $CenterContainer2/AttackButtons
 
 var inventory_visible := false
 var player_info_visible := false
 
 
 func _ready():
+	# Kết nối dialogue signal
 	dialogue_ui.connect("dialogue_advance", Callable(DialogueManager, "show_next"))
 	dialogue_ui.connect("dialogue_option_chosen", Callable(DialogueManager, "select_option"))	
 
@@ -19,13 +24,16 @@ func _process(_delta: float) -> void:
 func _input(event):
 	if event.is_action_pressed("ui_inventory"):
 		toggle_inventory()
-	if event.is_action_pressed("ui_player_info"):
+	elif event.is_action_pressed("ui_player_info"):
 		toggle_player_info()
 
 func toggle_inventory():
-	inventory_visible = not inventory_visible
+	inventory_visible = !inventory_visible
 	inventory_ui.visible = inventory_visible
 	
 func toggle_player_info():
-	player_info_visible = not player_info_visible
+	player_info_visible = !player_info_visible
 	player_info_ui.visible = player_info_visible
+	
+func show_shop(npc: NPC):
+	shop_ui.open_shop(npc)	
