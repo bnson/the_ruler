4cac7a16bd094e4303082d3bcddcdd0d8e58e8f2
bnson1986 <developer.bnson@gmail.com>
2025-08-17 21:51:31 +0700

    Initial commit

--- src/autoload/NpcInteractionManager.gd
+++ src/autoload/NpcInteractionManager.gd
@@ -16,11 +16,17 @@ func _on_node_added(node: Node) -> void:
 func _setup_npc(npc: NPC) -> void:
 	if not npc.player_entered.is_connected(_on_player_entered):
 		npc.player_entered.connect(_on_player_entered)
+	if not npc.player_exited.is_connected(_on_player_exited):
+		npc.player_exited.connect(_on_player_exited)
 	if not npc.request_dialogue.is_connected(_on_request_dialogue):
 		npc.request_dialogue.connect(_on_request_dialogue)
 
 func _on_player_entered(npc: NPC) -> void:
 	npc.interact_open_dialogue()
+
+func _on_player_exited(npc: NPC) -> void:
+	if DialogueManager.active and DialogueManager.npc_node == npc:
+		DialogueManager.end()
 
 func _on_request_dialogue(npc: NPC, res: DialogueResource, start_node_id: String) -> void:
 	DialogueManager.start(res, npc, start_node_id)
