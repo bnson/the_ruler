dd7914cbb7b97a62ea3bf55d4a62eca0d6f82080
BUI NGOC SON <bnson@live.com>
2025-08-21 14:18:34 +0700

    Update NPCInfo.gd

--- src/characters/npcs/NPCInfo.gd
+++ src/characters/npcs/NPCInfo.gd
@@ -4,11 +4,24 @@ class_name NPCInfo
 
 @export var npc_state: NPCState
 
-@onready var name_label := $VBoxContainer/NameLabel
-@onready var love_label := $VBoxContainer/LoveLabel
-@onready var trust_label := $VBoxContainer/TrustLabel
-@onready var favorite_list := $VBoxContainer/FavoriteGiftList
-@onready var given_list := $VBoxContainer/GivenGiftList
+@onready var name_label := $Main/Margin/HBox/LeftPanel/Margin/Container/Header/NameLabel
+@onready var level_label := $Main/Margin/HBox/LeftPanel/Margin/Container/Header/LevelLabel
+@onready var exp_value := $Main/Margin/HBox/LeftPanel/Margin/Container/Footer/ExpValue
+
+@onready var vitals_box_grid := $Main/Margin/HBox/RightPanel/Margin/Container/IndexPanel/VitalStatsBox/Grid
+@onready var hp_value : Label = vitals_box_grid.get_node("HpValue")
+@onready var mp_value : Label = vitals_box_grid.get_node("MpValue")
+@onready var sta_value : Label = vitals_box_grid.get_node("StaValue")
+@onready var love_value : Label = vitals_box_grid.get_node("LoveValue")
+@onready var trust_value : Label = vitals_box_grid.get_node("TrustValue")
+@onready var lust_value : Label = vitals_box_grid.get_node("LustValue")
+
+@onready var attributes_box_grid := $Main/Margin/HBox/RightPanel/Margin/Container/IndexPanel/BaseAttBox/Grid
+@onready var str_value : Label = attributes_box_grid.get_node("StrValue")
+@onready var dex_value : Label = attributes_box_grid.get_node("DexValue")
+@onready var int_value : Label = attributes_box_grid.get_node("IntValue")
+@onready var agi_value : Label = attributes_box_grid.get_node("AgiValue")
+
 
 func _ready():
 	if npc_state == null:
@@ -20,15 +33,22 @@ func _ready():
 
 	_update_info()
 
-	favorite_list.clear()
-	for gift in npc_state.favorite_gifts:
-		favorite_list.add_item(gift)
-
-	given_list.clear()
-	for gift in npc_state.given_gifts:
-		given_list.add_item(gift)
 
 func _update_info() -> void:
-	name_label.text = "Tên: " + npc_state.stats.name
-	love_label.text = "Tình cảm: " + str(npc_state.stats.love)
-	trust_label.text = "Tin tưởng: " + str(npc_state.stats.trust)
+	name_label.text = npc_state.stats.name
+	
+	var exp_to_next = npc_state.stats.get_exp_to_next_level(npc_state.stats.level)
+	level_label.text = str(npc_state.stats.level)
+	exp_value.text = "%d / %d" % [npc_state.stats.experience, exp_to_next]
+
+	hp_value.text = str(npc_state.stats.current_hp)
+	mp_value.text = str(npc_state.stats.current_mp)
+	sta_value.text = str(npc_state.stats.current_sta)
+	love_value.text = str(npc_state.stats.love)
+	trust_value.text = str(npc_state.stats.trust)
+	lust_value.text = str(npc_state.stats.lust)
+	
+	str_value.text = str(npc_state.stats.get_strength())
+	dex_value.text = str(npc_state.stats.get_dexterity())
+	int_value.text = str(npc_state.stats.get_intelligence())
+	agi_value.text = str(npc_state.stats.get_agility())
