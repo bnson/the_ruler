ae2c88474c04281ba9f80ead492eb011dc02673c
BUI NGOC SON <bnson@live.com>
2025-09-15 17:57:26 +0700

    Update PlayerDeathState.gd

--- src/characters/player/states/PlayerDeathState.gd
+++ src/characters/player/states/PlayerDeathState.gd
@@ -2,7 +2,14 @@ extends BaseState
 class_name PlayerDeathState
 
 func enter() -> void:
+	#var player = state_machine.get_parent()
+	#player.emit_signal("died")
+	#SystemDialogs.show_message("You have died xxx")
+	#player.respawn()
 	var player = state_machine.get_parent()
+	player.animation_state.travel("DeathState")
 	player.emit_signal("died")
+	await player.animation_tree.animation_finished
 	SystemDialogs.show_message("You have died")
 	player.respawn()
+	state_machine.change_state("IdleState")
