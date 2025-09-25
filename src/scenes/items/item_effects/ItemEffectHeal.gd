e973e896a6ec12f3e11f4cb1bc0a6357116b8787
BUI NGOC SON <bnson@live.com>
2025-09-25 18:05:48 +0700

    Update ItemEffectHeal.gd

--- src/scenes/items/item_effects/ItemEffectHeal.gd
+++ src/scenes/items/item_effects/ItemEffectHeal.gd
@@ -3,8 +3,8 @@ class_name  ItemEffectHeal extends ItemEffect
 @export var heal_amount : int = 1
 @export var sound : AudioStream
 
-var playerState : PlayerState
-
-func user() -> void:
-	if playerState.stats.current_hp > 0:
-		playerState.stats.current_hp = max(0, playerState.stats.current_hp + heal_amount)
+func use() -> void:
+	var playerStats = GameState.player.stats
+	print("Run item effect heal")
+	if playerStats.current_hp > 0:
+		playerStats.current_hp += heal_amount
