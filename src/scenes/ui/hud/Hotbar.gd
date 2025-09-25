2a8a59c3d197cace3423a75b351ad48c7e5ad5cd
BUI NGOC SON <bnson@live.com>
2025-09-25 18:03:39 +0700

    Update Hotbar.gd

--- src/scenes/ui/hud/Hotbar.gd
+++ src/scenes/ui/hud/Hotbar.gd
@@ -5,32 +5,45 @@ class_name Hotbar extends Control
 @onready var item_container : GridContainer = $Panel/ItemContainer
 
 func _ready():
-	GameState.connect("inventory_changed", Callable(self, "refresh"))
+	#GameState.connect("inventory_changed", Callable(self, "refresh"))
+	GameState.inventory_changed.connect(refresh)
 	refresh()
 	
 func refresh():
 	var inventory = GameState.player.inventory
 	var count_items := 0
+	var max_slots := 6
 	
 	for item in item_container.get_children():
 		item.queue_free()
 		
 	for data in inventory.get_all_items():
 		var item: Item = data["item"]
-		var slot = slot_scene.instantiate()
 	
-		if item.is_unique:
+		if item.effects.size() > 0:
+			var slot = slot_scene.instantiate()
 			item_container.add_child(slot)
+			slot.set_item(item, data["quantity"])
+			slot.connect("slot_clicked", Callable(self, "on_slot_clicked"))
 			count_items += 1
-
-		slot.set_item(item, data["quantity"])
-		slot.connect("slot_clicked", Callable(self, "on_slot_clicked"))
 		
 	# Thêm slot trống cho đủ max_slots
-	for i in range(inventory.max_slots - count_items):
+	for i in range(max_slots - count_items):
 		var slot = slot_scene.instantiate()
 		item_container.add_child(slot)
 		slot.set_item(null, 0)
 		
 func on_slot_clicked(clicked_slot):
-	pass
+	print("AAAAAAAAAAAAA");
+	for slot in item_container.get_children():
+		if slot.has_method("set_selected"):
+			slot.set_selected(slot == clicked_slot)
+			
+	var item = clicked_slot.current_item
+	if item:
+		if item.effects.size() > 0:
+			print("BBBBBB");
+			item.use()
+			GameState.player.inventory.remove_item(item, 1)
+			refresh()
+			
