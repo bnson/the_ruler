57aa7588f48f55d0ff7c7a5a7a7d8c58883d6554
BUI NGOC SON <bnson@live.com>
2025-09-25 15:13:37 +0700

    Update Item.gd

--- src/resources/items/Item.gd
+++ src/resources/items/Item.gd
@@ -1,10 +1,21 @@
-extends Resource
-class_name Item
+class_name Item extends Resource
 
 @export var id: String
 @export var name: String
-@export var description: String
+@export_multiline var description: String
 @export var type: String
 @export var price: int = 1
 @export var is_unique: bool = false
 @export var atlas_texture: AtlasTexture
+
+@export_category("Item Use Effects")
+@export var effects : Array[ItemEffect]
+
+func use() -> bool:
+	if effects.size() == 0:
+		return false
+		
+	for e in effects:
+		e.use()
+		
+	return true
