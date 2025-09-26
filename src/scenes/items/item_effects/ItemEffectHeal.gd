60de85c882f0bcc6c051948584a25aed9e4c6356
BUI NGOC SON <bnson@live.com>
2025-09-26 16:50:17 +0700

    Update ItemEffectHeal.gd

--- src/scenes/items/item_effects/ItemEffectHeal.gd
+++ src/scenes/items/item_effects/ItemEffectHeal.gd
@@ -1,10 +1,16 @@
-class_name  ItemEffectHeal extends ItemEffect
+class_name ItemEffectHeal extends ItemEffect
 
-@export var heal_amount : int = 10
-@export var sound : AudioStream
+@export var heal_percent : float = 1.0  # 1 nghĩa là 1%
+@export var audio_stream : AudioStream
 
-func use() -> void:
-	var playerStats = GameState.player.stats
-	print("Run item effect heal")
-	if playerStats.current_hp > 0:
-		playerStats.current_hp += heal_amount
+func use() -> bool:
+	var stats = GameState.player.stats
+	
+	if stats.current_hp > 0 and stats.current_hp < stats.max_hp:
+		var percent: float = clamp(heal_percent, 0.0, 100.0)
+		var heal_amount: int = floor(stats.max_hp * (percent / 100.0))
+		stats.current_hp = min(stats.current_hp + heal_amount, stats.max_hp)
+		AudioManager.play_sfx(audio_stream, 0.1)
+		return true
+	
+	return false
