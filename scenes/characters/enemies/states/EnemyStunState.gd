eb755c6608b2d8e60c476cd5e5710773d9d554a2
BUI NGOC SON <bnson@live.com>
2025-09-30 18:16:13 +0700

    Create EnemyStunState.gd

---
+++ scenes/characters/enemies/states/EnemyStunState.gd
@@ -0,0 +1,24 @@
+class_name EnemyStunState extends BaseState
+
+func enter() -> void:
+	print("Enemy stun state enter...")
+	state_machine.get_parent().animation_state.travel("StunState")
+
+func physics_update(_delta: float, _input_vector := Vector2.ZERO) -> void:
+	var enemy = state_machine.get_parent()
+	var player = enemy.get_player()
+
+	if not player:
+		state_machine.change_state("PatrolState")
+		return
+
+	var distance = enemy.global_position.distance_to(player.global_position)
+
+	if distance < 10:
+		state_machine.change_state("AttackState")
+	elif distance > 200:
+		state_machine.change_state("PatrolState")
+	else:
+		var direction = (player.global_position - enemy.global_position).normalized()
+		enemy.velocity = direction * enemy.speed
+		enemy.move_and_slide()
