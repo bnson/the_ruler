bfae99c59b75786edbd9d15aab93ed844d1ac30b
BUI NGOC SON <bnson@live.com>
2026-01-02 14:48:08 +0700

    Create ActionPanel.gd

---
+++ scenes/components/panel/ActionPanel.gd
@@ -0,0 +1,12 @@
+class_name ActionPanel
+extends Control
+
+#=======================================================
+# Các nút hành động
+@onready var btn_attack = $ButtonAttack
+
+#=======================================================
+func _ready() -> void:
+	print("Action panel ready...")
+
+#=======================================================
