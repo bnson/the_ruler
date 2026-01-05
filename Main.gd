4948aad0bd72d0e07a752920e6637f16e4446a80
BUI NGOC SON <bnson@live.com>
2026-01-05 15:56:51 +0700

    Create Main.gd

---
+++ Main.gd
@@ -0,0 +1,17 @@
+class_name Main
+extends Node
+
+#==============================================================
+@onready var ui_layer = $UiLayer
+@onready var fade_layer = $FadeLayer
+@onready var scene_container = $SceneContainer
+
+
+#==============================================================
+func _ready():
+	# Đăng ký các layer với UiManager
+	UiManager.register_ui(ui_layer)
+	# Load game
+	SaveManager.load_game(scene_container, fade_layer)
+	# Bật auto-save định kỳ
+	SaveManager.auto_save_start()
