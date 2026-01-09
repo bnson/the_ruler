460ecfb278b0e4b0c65a11a340c35557c66a9f7d
BUI NGOC SON <bnson@live.com>
2026-01-09 17:23:15 +0700

    Update UiLayer.gd

--- scenes/ui/UiLayer.gd
+++ scenes/ui/UiLayer.gd
@@ -6,6 +6,7 @@ extends CanvasLayer
 @onready var action_panel: ActionPanel = $BottomRightUi/ActionPanel
 @onready var hud_panel: HudPanel = $TopLeftUi/HudPanel
 @onready var inventory_panel: InventoryPanel = $CenterUi/InventoryPanel
+@onready var player_profile: PlayerProfile = $CenterUi/PlayerProfile
 
 #========================================================
 func _ready():
@@ -15,9 +16,17 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("inventory_panel"):
 		toggle_inventory()
+	if event.is_action_pressed("player_profile"):
+		toggle_player_profile()
 
 func toggle_inventory():
 	if inventory_panel.visible:
 		inventory_panel.visible = false
 	else:
 		inventory_panel.visible = true
+		
+func toggle_player_profile():
+	if player_profile.visible:
+		player_profile.visible = false
+	else:
+		player_profile.visible = true
