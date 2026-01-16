3a93d110938a9b560efef7e1301273b628ddf085
BUI NGOC SON <bnson@live.com>
2026-01-16 17:52:34 +0700

    Update UiLayer.gd

--- scenes/ui/UiLayer.gd
+++ scenes/ui/UiLayer.gd
@@ -8,10 +8,27 @@ extends CanvasLayer
 @onready var inventory_panel: InventoryPanel = $CenterUi/InventoryPanel
 @onready var player_profile: PlayerProfile = $CenterUi/PlayerProfile
 @onready var interaction_menu: InteractionMenu = $CenterUi/InteractionMenu
+@onready var shop_ui: ShopUi = $CenterUi/ShopUi
 
 #========================================================
 func _ready():
+	interaction_menu.interaction_chosen.connect(on_interaction_chosen)
 	pass
+
+#========================================================
+func on_interaction_chosen(npc: Npc, interaction: Dictionary) -> void:
+	# Tắt menu tương tác
+	interaction_menu.hide()
+	
+	var action = interaction.get("action", "")
+	print("Action: ", action)
+	match action:
+		"open_shop":
+			shop_ui.handle_interaction(npc)
+		"open_chat":
+			pass
+		_:
+			print("Action không hỗ trợ:", interaction)
 
 #========================================================
 func _input(event):
