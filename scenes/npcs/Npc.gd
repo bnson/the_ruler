1552050c55b8c82efa713666ab9b54354b95af85
BUI NGOC SON <bnson@live.com>
2026-01-16 17:38:38 +0700

    Add Npc class with properties for display and interactions

---
+++ scenes/npcs/Npc.gd
@@ -0,0 +1,7 @@
+class_name Npc
+extends CharacterBody2D
+
+@export var display_name: String
+@export_multiline var display_info: String
+@export var sell_items: Array[ItemData]
+@export var interactions: Array[Dictionary]
