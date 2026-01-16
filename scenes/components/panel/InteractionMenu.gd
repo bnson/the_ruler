485e233b7f6342368e7b71a9305caae3c4db1184
BUI NGOC SON <bnson@live.com>
2026-01-16 18:09:13 +0700

    Update InteractionMenu.gd

--- scenes/components/panel/InteractionMenu.gd
+++ scenes/components/panel/InteractionMenu.gd
@@ -1,6 +1,8 @@
 class_name InteractionMenu
 extends Control
 
+signal interaction_chosen(npc: Npc)
+
 @export var button_scene: PackedScene
 
 @onready var title_label = $Margin/Main/Margin/Panel/Margin/VBox/TitleLabel
@@ -10,20 +12,25 @@ extends Control
 func _ready():
 	pass
 
-func open(interactions: Array[Dictionary]):
+func open(npc: Npc):
 	queue_free_children(interactions_container)
 	interactions_container.visible = true
 	interactions_container.columns = 1
 	
-	for interaction in interactions:
+	for interaction in npc.interactions:
 		var btn := button_scene.instantiate()
 		btn.text = interaction.get("name", "...")
+		btn.pressed.connect(func(): on_button_pressed(npc, interaction))
 		interactions_container.add_child(btn)
 	
 		show()
 
 func close():
 	hide()
+
+func on_button_pressed(npc: Npc, interaction: Dictionary):
+	interactions_container.visible = false
+	emit_signal("interaction_chosen", npc, interaction)
 
 func queue_free_children(node: Node) -> void:
 	for child in node.get_children():
