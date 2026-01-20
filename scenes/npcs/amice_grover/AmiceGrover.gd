9fcae8cbd8f9b691b49c80a338fa44f30da32763
BUI NGOC SON <bnson@live.com>
2026-01-20 17:03:10 +0700

    Update AmiceGrover.gd

--- scenes/npcs/amice_grover/AmiceGrover.gd
+++ scenes/npcs/amice_grover/AmiceGrover.gd
@@ -8,12 +8,23 @@ func _ready() -> void:
 	pass
 
 func _on_perception_area_entered(area: Area2D) -> void:
-	if area.get_owner().is_in_group("player"):
-		print("Amice detects the player.")
+	if not is_instance_valid(area):
+		return
+	
+	var area_owner := area.get_owner()
+	if not is_instance_valid(area_owner):
+		return
+	
+	if area_owner.is_in_group("player"):
 		UiManager.ui_layer.interaction_menu.open(self)
 
 func _on_perception_area_exited(area: Area2D) -> void:
-	if area:
-		if area.get_owner().is_in_group("player"):
-			print("Amice did not detect the player.")
-			UiManager.ui_layer.interaction_menu.close()
+	if not is_instance_valid(area):
+		return
+	
+	var area_owner := area.get_owner()
+	if not is_instance_valid(area_owner):
+		return
+	
+	if area_owner.is_in_group("player"):
+		UiManager.ui_layer.interaction_menu.close()
