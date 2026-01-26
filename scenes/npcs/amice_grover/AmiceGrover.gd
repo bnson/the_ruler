dd02594bb966fd4155d8471fa54b182ed25d5fed
BUI NGOC SON <bnson@live.com>
2026-01-26 15:25:51 +0700

    Update AmiceGrover.gd

--- scenes/npcs/amice_grover/AmiceGrover.gd
+++ scenes/npcs/amice_grover/AmiceGrover.gd
@@ -3,13 +3,12 @@ extends Npc
 
 @onready var detection_area: Area2D = $Perception
 
+
 func _ready() -> void:
 	super()
 	stats = $Stats
-	#roll_mood()
-	#load_ask_from_bank()
+	NpcManager.register(self)
 	
-
 func _on_perception_area_entered(area: Area2D) -> void:
 	if not is_instance_valid(area):
 		return
@@ -32,3 +31,12 @@ func _on_perception_area_exited(area: Area2D) -> void:
 	
 	if area_owner.is_in_group("player"):
 		UiManager.ui_layer.interaction_menu.close()
+
+func exit_tree() -> void:
+	NpcManager.unregister(self)
+
+func to_save_data() -> Dictionary:
+	return stats.to_dict()
+
+func load_save_data(data: Dictionary) -> void:
+	stats.from_dict(data)
